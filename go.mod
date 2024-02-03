d8d64c86db1b00c53e18afa15fd5ceca9af76654
Pavel Druzhinin <paveldroo@gmail.com>
2024-02-03 21:03:23 +0300

    Add session logging and auth

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-photo-blog
 
 go 1.21
+
+require github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/google/uuid"
 	"html/template"
 	"net/http"
 )
@@ -11,12 +12,51 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
-func main() {
-	http.HandleFunc("/", index)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
-	http.ListenAndServe(":8080", nil)
+type user struct {
+	UserName string
+	Email    string
 }
 
-func index(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+var dbSessions = make(map[string]user)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/login", login)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session")
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	u, ok := dbSessions[c.Value]
+	if !ok {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		u := user{
+			UserName: r.FormValue("username"),
+			Email:    r.FormValue("email"),
+		}
+		v := uuid.NewString()
+		http.SetCookie(w, &http.Cookie{Name: "session", Value: v, HttpOnly: true})
+		dbSessions[v] = u
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	_, err := r.Cookie("session")
+	if err == nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "login.gohtml", nil)
 }
